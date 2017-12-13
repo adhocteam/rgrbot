cec017e7ead51398db6fbaafaff6ef293fcf28da
Mickin Sahni <mickinsahni@gmail.com>
2017-12-13 15:38:58 -0500

    Update response plan
    Added QPP Incident response plan link and fixed res.send()

--- scripts/incident_response.js
+++ scripts/incident_response.js
@@ -3,20 +3,19 @@ const incident_plan = `
 
 *2. Create a copy of the QPPFE Incident Response Template:* https://docs.google.com/document/d/1d1vjbKokJ7ogFGPItrDj7xtXoOHLo-aSv7kFz6JKhKU/copy
 
-*3. Report the active incident to Product Owner and appropriate team(s) as needed:*
+*3. Report the active incident to Product Owner and appropriate team(s) as needed (see Resources below):*
 - PagerDuty
-- Email
--- Subject line = "[Active Incident] {description}"
--- To = 
-`Koh, David (CMS/) <David.Koh@cms.hhs.gov>; Haselton, Scott (CMS/OA) <Scott.Haselton@cms.hhs.gov>; lucas.brown@cms.hhs.gov <lucas.brown@cms.hhs.gov>; Verch, Shaun (CMS/CTR) <Shaun.Verch@cms.hhs.gov>; Walter, Stephen J. (CMS/OEDA) <stephen.walter@cms.hhs.gov>; Natalie Medler <natalie.medler@us.pwc.com>; Woods, Shamika <shwoods@qssinc.com>; Kahler, Constance <ckahler@qssinc.com>; Slay, Amanda <Amanda.Slay@ventech.hcqis.org>; Sanchez, Yadira (CMS/OEDA) <Yadira.Sanchez@cms.hhs.gov>; Abrams, Adrien (CMS/OEDA) <Adrien.Abrams@cms.hhs.gov>; Heller, Adam  (CMS/CCSQ) <Adam.Heller@cms.hhs.gov>; Ostrow, Stan (CMS/CCSQ) <Stan.Ostrow@cms.hhs.gov>`
+- Email 
 - HipChat
 - Slack
 
 *4. Establish lines of communication (video and/or phone)*
+
+*Resources:* QPP's Incident Response Plan Documentation
 `;
 
 module.exports = function (robot) {
     robot.hear(/activate incident response/i, function (res) {
-        res.send(values);
+        res.send(incident_plan);
     });
 };  
